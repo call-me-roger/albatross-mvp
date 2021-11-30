b44dc30d097e97d7757577bb94efc146050ecc27
Rogerio Mendes <rogerio.mendes@rookau.com>
2021-11-30 00:23:43 -0300

    New Marketplace contract

--- src/contracts/Marketplace.js
+++ src/contracts/Marketplace.js
@@ -1,5 +1,10 @@
+import { provider } from 'constants/provider'
 import { ethers } from 'ethers'
+import { getNFTDetails } from './GolfClub'
+import { getNFTReadContract } from './GolfClub'
 
+const MARKETPLACE_CONTRACT_ADDRESS =
+  '0x2E0c050459aea0d5767E1007ad00d714BB899Ba6'
 const SOL_MARKETPLACE_ABI = [
   {
     anonymous: false,
@@ -142,6 +147,25 @@ const SOL_MARKETPLACE_ABI = [
         type: 'uint256',
       },
     ],
+    name: 'listingIndexToToken',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
     name: 'listings',
     outputs: [
       {
@@ -172,6 +196,19 @@ const SOL_MARKETPLACE_ABI = [
       },
     ],
     name: 'listingsBalance',
+    outputs: [
+      {
+        internalType: 'uint256',
+        name: '',
+        type: 'uint256',
+      },
+    ],
+    stateMutability: 'view',
+    type: 'function',
+  },
+  {
+    inputs: [],
+    name: 'nextListingId',
     outputs: [
       {
         internalType: 'uint256',
@@ -243,10 +280,88 @@ const SOL_MARKETPLACE_ABI = [
   },
 ]
 
-const MARKETPLACE_ABI = null
+const MARKETPLACE_ABI = [
+  'event ListingUpdate(uint256 _golfClubId, uint256 _price, address _seller, bool _active)',
+  'event OwnershipTransferred(address indexed previousOwner, address indexed newOwner)',
+  'function addListing(uint256 _golfClubId, uint256 _price)',
+  'function cancelListing(uint256 _golfClubId)',
+  'function claimBalance(address) view returns (uint256)',
+  'function claimOwnerBalance()',
+  'function getToken() view returns (address)',
+  'function isOwner() view returns (bool)',
+  'function listingIndexToToken(uint256) view returns (uint256)',
+  'function listings(uint256) view returns (uint256 price, address seller, bool active)',
+  'function listingsBalance(address) view returns (uint256)',
+  'function nextListingId() view returns (uint256)',
+  'function owner() view returns (address)',
+  'function purchase(uint256 _golfClubId) payable',
+  'function renounceOwnership()',
+  'function setGolfClubContract(address _contractAddress)',
+  'function transferOwnership(address newOwner)',
+]
+
 export function convertABI() {
   const newABI = new ethers.utils.Interface(SOL_MARKETPLACE_ABI)
 
   return newABI.format()
 }
-console.log('marketplace===', convertABI())
+//console.log('marketplaceABI', convertABI())
+
+function getSignedContract(signer) {
+  return new ethers.Contract(
+    MARKETPLACE_CONTRACT_ADDRESS,
+    MARKETPLACE_ABI,
+    signer,
+  )
+}
+
+function getReadContract() {
+  return new ethers.Contract(
+    MARKETPLACE_CONTRACT_ADDRESS,
+    MARKETPLACE_ABI,
+    provider,
+  )
+}
+
+export async function getListings() {
+  const signer = provider.getSigner()
+  const address = await signer.getAddress()
+  const listedTokenIds = []
+
+  if (address) {
+    const contract = getReadContract()
+    const nextListingId = await contract.nextListingId()
+    const totalListings = nextListingId.toNumber()
+    for (let i = 0; i < totalListings; i++) {
+      const bigNumberTokenId = await contract.listingIndexToToken(i)
+      const tokenId = bigNumberTokenId.toNumber()
+      if (listedTokenIds.indexOf(tokenId) === -1) listedTokenIds.push(tokenId)
+    }
+  }
+
+  return listedTokenIds
+}
+
+export async function getListedTokens() {
+  const listedTokenIds = await getListings()
+  const result = []
+  const contract = getReadContract()
+  const nftContract = getNFTReadContract()
+
+  await Promise.all(
+    listedTokenIds.map(async tokenId => {
+      const listedData = await contract.listings(tokenId)
+      if (listedData?.active) {
+        result.push({
+          listing: {
+            ...listedData,
+            price: ethers.utils.formatEther(listedData.price),
+          },
+          nft: await getNFTDetails(nftContract, tokenId),
+        })
+      }
+    }),
+  )
+
+  return result
+}
